761e7fbe4e23198d322e9f9a9f4d70e30425fbee
Raft of Penguins <raftofpenguins@gmail.com>
2021-06-30 17:27:04 -0700

    Applied script to Bird and added code so it no longer falls by default

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,7 +7,7 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
     // Update is called once per frame
