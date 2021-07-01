1f1b958c786483345db8321e1c882f52f49b3f31
Raft of Penguins <raftofpenguins@gmail.com>
2021-06-30 17:58:46 -0700

    Added launch function

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    Vector2 _startPosition; // Defines variable for use in Start function
+
     // Start is called before the first frame update
     void Start()
     {
+        _startPosition = GetComponent<Rigidbody2D>().position;
         GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
@@ -17,6 +20,13 @@ public class Bird : MonoBehaviour
 
     void OnMouseUp()
     {
+        Vector2 currentPosition = GetComponent<Rigidbody2D>().position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddForce(direction * 500);
+
         GetComponent<SpriteRenderer>().color = Color.white;
     }
 
